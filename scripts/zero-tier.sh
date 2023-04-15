0ce7acb4a669164eb86106b96556fdf6e4544b61
Raul Dipeas <rdipeas+github@proton.me>
2023-04-15 18:47:46 -0300

    +zero-tier

---
+++ scripts/zero-tier.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+# Instalação do ZeroTier
+wget -qO- https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/zerotier.gpg>/dev/null
+echo 'deb http://download.zerotier.com/debian/jammy jammy main'|sudo tee /etc/apt/sources.list.d/zerotier.list>/dev/null
+pkcon install zerotier-one
