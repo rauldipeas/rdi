b4269a1d291c88f223fc0b92c13983b205126f2c
Raul Dipeas <rdipeas+github@proton.me>
2023-04-15 15:44:12 -0300

    +mangohud

---
+++ scripts/mangohud.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Instalação do MangoHud
+wget -qc "$(wget -qO- https://api.github.com/repos/flightlessmango/MangoHud/releases|grep browser_download_url|grep download/v|grep tar.gz|head -n1|cut -d '"' -f4)"
+tar fxz MangoHud*.tar.gz
+cd MangoHud
+./mangohud-setup.sh install
+cd ..
+rm -rf MangoHud
