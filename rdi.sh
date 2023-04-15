fd8158e8b04d970a83fe1e9cd2b33d6ad665f199
Raul Dipeas <rdipeas+github@proton.me>
2023-04-15 19:17:55 -0300

    ^rdi

--- rdi.sh
+++ rdi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 mkdir -p "$HOME"/.rdi/logs
-bash <(https://raw.githubusercontent.com/rauldipeas/rdi/main/scripts/"$@".sh)|tee "$HOME"/.rdi/logs/"$@"-"$(ls -A $HOME/.rdi/logs/$@* 2>/dev/null|wc -l)".log
+bash <(wget -qO- https://raw.githubusercontent.com/rauldipeas/rdi/main/scripts/"$@".sh)|tee "$HOME"/.rdi/logs/"$@"-"$(ls -A $HOME/.rdi/logs/$@* 2>/dev/null|wc -l)".log
