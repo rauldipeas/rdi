3c8193a09e03d100ab93e23013fc0380c2b4b14e
Raul Dipeas <rdipeas+github@proton.me>
2023-04-15 14:35:46 -0300

    +jekyll

---
+++ scripts/jekyll.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# Instalação do Ruby
+sudo apt install ruby-dev
+
+# Configuração do ambiente Ruby
+cat <<EOF |tee "$HOME"/.bashrc.d/ruby.bash>/dev/null
+export GEM_HOME="\$HOME/.gems"
+export PATH="\$HOME/.gems/bin:\$PATH"
+EOF
+source "$HOME"/.bashrc
+
+# Instalação do Jekyll
+gem install bundler jekyll
+
+# Teste opcional
+#jekyll new my-awesome-site
+#cd my-awesome-site
+#bundle exec jekyll serve&sleep 3&&xdg-open http://localhost:4000
