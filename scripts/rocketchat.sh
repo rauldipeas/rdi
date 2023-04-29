e7440431c777689fd89d01eeec59a1da281b2541
Raul Dipeas <rdipeas+github@proton.me>
2023-04-29 00:33:20 -0300

    ^rocketchat

--- scripts/rocketchat.sh
+++ scripts/rocketchat.sh
@@ -7,3 +7,10 @@ pkcon install deb-get
 
 # Instalação do Rocket Chat
 deb-get install rocketchat
+
+# Correção dos ícones na área de notificação
+if [ -d /usr/share/icons/Papirus ]; then
+    if [ -f /usr/bin/hardcode-tray ]; then
+        sudo hardcode-tray --apply --theme Papirus-Dark
+    fi
+fi
