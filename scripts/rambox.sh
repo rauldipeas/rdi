274c75128337933728a396067615981f179bd89a
Raul Dipeas <rdipeas+github@proton.me>
2023-05-01 21:14:06 -0300

    +rambox

---
+++ scripts/rambox.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Instalação do deb-get
+bash <(wget -qO- https://raw.githubusercontent.com/rauldipeas/apt-repository/main/apt-repository.sh)
+pkcon install deb-get
+
+# Instalação do Rambox
+deb-get install rambox
