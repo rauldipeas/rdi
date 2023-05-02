80dcc96e2843bd810f838617f78f3782047988c1
Raul Dipeas <rdipeas+github@proton.me>
2023-05-01 21:16:07 -0300

    ^pipx

--- scripts/pipx.sh
+++ scripts/pipx.sh
@@ -4,7 +4,7 @@ set -e
 # Instalação do pipx
 pkcon install pipx
 sudo mkdir -p /opt/pipx/bin
-sudo chmod -R 777 /opt/pipx
+sudo chmod 777 -R /opt/pipx
 cat <<EOF |sudo tee /etc/X11/Xsession.d/99pipx
 export PIPX_HOME='/opt/pipx'
 export PIPX_BIN_DIR='/opt/pipx/bin'
