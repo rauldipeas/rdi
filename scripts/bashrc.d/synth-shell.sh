81af1455128e7051a7b952123c79f33480a33ede
Raul Dipeas <rdipeas+github@proton.me>
2023-05-06 01:39:40 -0300

    ^synth-shell

--- scripts/bashrc.d/synth-shell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Instalação do synth-shell
-pkcon install git
-git clone --recursive https://github.com/andresgongora/synth-shell.git
-bash synth-shell/setup.sh
-rm -rf synth-shell
-source "$HOME"/.bashrc
