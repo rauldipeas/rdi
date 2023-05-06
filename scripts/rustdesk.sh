b1307ab8be950f3a72301c4fbf2f7ae81ca4ef5b
Raul Dipeas <rdipeas+github@proton.me>
2023-05-06 01:38:54 -0300

    ^rustdesk

--- scripts/rustdesk.sh
+++ scripts/rustdesk.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Instalação do deb-get
-bash <(wget -qO- https://raw.githubusercontent.com/rauldipeas/apt-repository/main/apt-repository.sh)
-pkcon install deb-get
-
 # Instalação do RustDesk
-deb-get install rustdesk
+bash <(wget -qO- https://raw.githubusercontent.com/rauldipeas/apt-repository/main/apt-repository.sh)
+pkcon install rustdesk
