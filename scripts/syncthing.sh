b6ba16a89f18db242caabe023edae9bba8af7fba
Raul Dipeas <rdipeas+github@proton.me>
2023-05-06 01:21:52 -0300

    ^syncthing

--- scripts/syncthing.sh
+++ scripts/syncthing.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-# Instalação do deb-get
-bash <(wget -qO- https://raw.githubusercontent.com/rauldipeas/apt-repository/main/apt-repository.sh)
-pkcon install deb-get
-
 # Instalação do SyncThing
-deb-get install syncthing
+wget -qO- https://syncthing.net/release-key.gpg|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/syncthing-archive-keyring.gpg
+echo 'deb https://apt.syncthing.net/ syncthing stable' | sudo tee /etc/apt/sources.list.d/syncthing.list
+pkcon refresh
+pkcon install syncthing
