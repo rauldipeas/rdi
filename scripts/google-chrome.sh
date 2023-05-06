aff03ff17ad9fe69aad8a6b02238a01f471009d8
Raul Dipeas <rdipeas+github@proton.me>
2023-05-06 10:06:42 -0300

    +google-chrome

---
+++ scripts/google-chrome.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Instalação do Google Chrome
+bash <(wget -qO- https://raw.githubusercontent.com/rauldipeas/apt-repository/main/apt-repository.sh)
+pkcon install google-chrome
+
+# Correção dos ícones na área de notificação
+if [ -d /usr/share/icons/Papirus ]; then
+    if [ -f /usr/bin/hardcode-tray ]; then
+        sudo hardcode-tray --apply --theme Papirus-Dark
+    fi
+fi
