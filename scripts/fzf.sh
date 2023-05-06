2060cbfbef171b58c18231e44d07284f01d8c020
Raul Dipeas <rdipeas+github@proton.me>
2023-05-06 01:37:24 -0300

    ^fzf

---
+++ scripts/fzf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# Instalação do fzf
+bash <(wget -qO- https://raw.githubusercontent.com/rauldipeas/apt-repository/main/apt-repository.sh)
+pkcon install bat fzf
+cat <<EOF |sudo tee /usr/local/bin/fzf-dir>/dev/null
+#!/bin/bash
+set -e
+/usr/bin/fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'
+EOF
+sudo chmod +x /usr/local/bin/fzf-dir
