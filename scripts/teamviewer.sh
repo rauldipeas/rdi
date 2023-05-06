5d65c14ef6d1ad8f8df16691db4fc8b292d38573
Raul Dipeas <rdipeas+github@proton.me>
2023-05-06 01:39:11 -0300

    -teamviewer

--- scripts/teamviewer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Instalação do deb-get
-bash <(wget -qO- https://raw.githubusercontent.com/rauldipeas/apt-repository/main/apt-repository.sh)
-pkcon install deb-get
-
-#Instalação do TeamViewer
-deb-get install teamviewer
