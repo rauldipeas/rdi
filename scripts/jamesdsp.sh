7deab1f88bfdcba7d5c63e6acd3513b802c48ccc
Raul Dipeas <rdipeas+github@proton.me>
2023-05-17 10:53:10 -0300

    ^jamedsp

--- scripts/jamesdsp.sh
+++ scripts/jamesdsp.sh
@@ -2,12 +2,12 @@
 set -e
 
 # Instalação do JamesDSP
-wget -qO- https://thepbone.github.io/PPA-Repository/KEY.gpg|sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/thepbone_ppa.gpg
+wget -qO- https://thepbone.github.io/PPA-Repository/KEY.gpg|sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/thepbone_ppa.gpg>/dev/null
 echo 'deb [arch=amd64] https://thepbone.github.io/PPA-Repository ./'|sudo tee /etc/apt/sources.list.d/thepbone_ppa.list>/dev/null
 pkcon refresh
 pkcon install jamesdsp-pulse
 
 # Correção no ícone do lançador
-cat <<EOF| sudo tee /etc/apt/apt.conf.d/100jamesdsp-icon>/dev/null
+cat <<EOF |sudo tee /etc/apt/apt.conf.d/100jamesdsp-icon>/dev/null
 DPkg::Post-Invoke {"sed -i 's@Icon=/usr/share/pixmaps/jamesdsp.png@Icon=jamesdsp@g' /usr/share/applications/jamesdsp.desktop";};
 EOF
