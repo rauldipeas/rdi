21dbdee70293c6e3d42050516c02d253f37947b8
Raul Dipeas <rdipeas+github@proton.me>
2023-05-17 03:12:07 -0300

    +blender

---
+++ scripts/blender.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+# Instalação do Blender
+BLENDER_VER=$(wget -qO- https://ftp.nluug.nl/pub/graphics/blender/release|grep Blender3.|tail -n1|cut -d \" -f6)
+BLENDER_FILE=$(wget -qO- https://ftp.nluug.nl/pub/graphics/blender/release/"$BLENDER_VER"|grep .tar.xz|tail -n1|cut -d \" -f6)
+wget -c https://ftp.nluug.nl/pub/graphics/blender/release/"$BLENDER_VER""$BLENDER_FILE"
+tar -xf blender*.tar.xz
+sudo mkdir -p /opt/blender
+sudo chmod 777 /opt/blender
+sudo mv blender*/* /opt/blender/
+rm -r blender*/ blender*.tar.xz
+sudo mkdir -p /usr/local/bin /usr/local/share/applications /usr/local/share/pixmaps
+sudo ln -sf /opt/blender/blender /usr/local/bin/blender
+sudo cp /opt/blender/blender.desktop /usr/local/share/applications/blender.desktop
+sudo ln -sf /opt/blender/blender.svg /usr/local/share/pixmaps/blender.svg
