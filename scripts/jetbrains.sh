356d46539b477c6165fafa8b3b2295f9e078ee71
Raul Dipeas <rdipeas+github@proton.me>
2023-05-17 10:53:19 -0300

    +jetbrains

---
+++ scripts/jetbrains.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# Instalação do JetBrains
+wget -qO- https://s3.eu-central-1.amazonaws.com/jetbrains-ppa/0xA6E8698A.pub.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/jetbrains-ppa-archive-keyring.gpg>/dev/null
+echo 'deb http://jetbrains-ppa.s3-website.eu-central-1.amazonaws.com any main'|sudo tee /etc/apt/sources.list.d/jetbrains-ppa.list>/dev/null
+pkcon refresh
+#pkcon install\
+#    clion\
+#    datagrip\
+#    dataspell\
+#    goland\
+#    intelij-idea-community\
+#    intelij-idea-ultimate\
+#    jetbrains-gateway\
+#    phpstorm\
+#    pycharm-community\
+#    pycharm-education\
+#    pycharm-profressional\
+#    rider\
+#    rubymine\
+#    webstorm
