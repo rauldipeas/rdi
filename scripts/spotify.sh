57bedb8509cb9d02ce32267a8b9dbde12582a8b5
Raul Dipeas <rdipeas+github@proton.me>
2023-05-17 03:09:58 -0300

    +spotify

---
+++ scripts/spotify.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+# Instalação do Spotify
+wget -qO- https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg|sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo 'deb http://repository.spotify.com stable non-free'|sudo tee /etc/apt/sources.list.d/spotify.list
+pkcon refresh
+pkcon install spotify-client
