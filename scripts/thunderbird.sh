c27180b9fb74886cabaa9ad714c65697aad65332
Raul Dipeas <rdipeas+github@proton.me>
2023-05-20 03:57:28 -0300

    ^thunderbird

--- scripts/thunderbird.sh
+++ scripts/thunderbird.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Instalação do Thunderbird
-wget -q --show-progress -O thunderbird-latest-linux64-pt-br.tar.bz2 "https://download.mozilla.org/?product=thunderbird-latest&os=linux64&lang=pt-BR"
+wget -cq --show-progress -O thunderbird-latest-linux64-pt-br.tar.bz2 'https://download.mozilla.org/?product=thunderbird-latest&os=linux64&lang=pt-BR'
 tar fjx thunderbird-latest-linux64-pt-br.tar.bz2
 sudo mkdir -p /opt/thunderbird
 sudo chmod 777 /opt/thunderbird
@@ -17,7 +17,7 @@ Name=Thunderbird
 Comment=Envie e receba e-mails com o Thunderbird
 GenericName=Cliente de e-mail
 Keywords=Email;E-mail;Newsgroup;Feed;RSS
-Exec=thunderbird %u
+Exec=thunderbird-kdocker %u
 Terminal=false
 X-MultipleArgs=false
 Type=Application
@@ -25,23 +25,35 @@ Icon=thunderbird
 Categories=Application;Network;Email;
 MimeType=x-scheme-handler/mailto;application/x-xpinstall;
 StartupNotify=true
-Actions=Compose;Contacts
+Actions=Compose
 
 [Desktop Action Compose]
 Name=Compor uma nova mensagem
 Exec=thunderbird -compose
-
-[Desktop Action Contacts]
-Name=Contatos
-Exec=thunderbird -addressbook
 EOF
 
+# KDocker
+pkcon install kdocker
+cat <<EOF |tee /opt/thunderbird/thunderbird-kdocker>/dev/null
+#!/bin/bash
+set -e
+thunderbird "\$@"&
+if [ "\$(pgrep kdocker)" ];then
+	killall -9 kdocker
+	kdocker -i /usr/share/icons/Papirus/22x22/panel/thunderbird.svg	-mq thunderbird
+	else
+	kdocker -i /usr/share/icons/Papirus/22x22/panel/thunderbird.svg	-mq thunderbird
+fi
+EOF
+sudo chmod +x /opt/thunderbird/thunderbird-kdocker
+sudo ln -fs /opt/thunderbird/thunderbird-kdocker /usr/local/bin/thunderbird-kdocker
+
 # Instalação do Birdtray
-wget -q --show-progress https://ppa.launchpadcontent.net/linuxuprising/ppa/ubuntu/pool/main/b/birdtray/$(wget -qO- https://ppa.launchpadcontent.net/linuxuprising/ppa/ubuntu/pool/main/b/birdtray/|grep jammy|grep amd64.deb|cut -d '"' -f8|tail -n1)
-dpkg-deb -x birdtray*.deb birdtray
-sudo cp birdtray/usr/bin/birdtray /opt/thunderbird/
-sudo ln -fs /opt/thunderbird/birdtray /usr/local/bin/birdtray
-sudo cp birdtray/usr/share/applications/com.ulduzsoft.Birdtray.desktop /usr/local/share/applications/
-sudo cp -r birdtray/usr/share/icons /usr/loca/share/
-sudo cp -r birdtray/usr/share/ulduzsoft /usr/local/share/
-wget -q --show-progress -O "$HOME"/.config/birdtray-config.json https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/birdtray/birdtray-config.json
+#wget -cq --show-progress https://ppa.launchpadcontent.net/linuxuprising/ppa/ubuntu/pool/main/b/birdtray/$(wget -qO- https://ppa.launchpadcontent.net/linuxuprising/ppa/ubuntu/pool/main/b/birdtray/|grep jammy|grep amd64.deb|cut -d '"' -f8|tail -n1)
+#dpkg-deb -x birdtray*.deb birdtray
+#sudo cp birdtray/usr/bin/birdtray /opt/thunderbird/
+#sudo ln -fs /opt/thunderbird/birdtray /usr/local/bin/birdtray
+#sudo cp birdtray/usr/share/applications/com.ulduzsoft.Birdtray.desktop /usr/local/share/applications/
+#sudo cp -r birdtray/usr/share/icons /usr/loca/share/
+#sudo cp -r birdtray/usr/share/ulduzsoft /usr/local/share/
+#wget -cq --show-progress -O "$HOME"/.config/birdtray-config.json https://raw.githubusercontent.com/rauldipeas/respin-rdx/main/assets/birdtray/birdtray-config.json
