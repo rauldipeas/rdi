9914a8efc8d2b02451a21c4040c230e4b796fcaa
Raul Dipeas <rdipeas+github@proton.me>
2023-05-20 03:57:43 -0300

    ^mcfly

--- scripts/bashrc.d/mcfly.sh
+++ scripts/bashrc.d/mcfly.sh
@@ -5,7 +5,7 @@ set -e
 #xdg-open https://github.com/rauldipeas/rdi/blob/main/scripts/bash-enhancements.sh
 
 # Instalação do mcfly
-wget -cq --show-progress $(wget -qO- https://api.github.com/repos/cantino/mcfly/releases|grep browser_download_url|grep download|grep x86_64|grep linux|head -n1|cut -d '"' -f4)
+wget -cq --show-progress "$(wget -qO- https://api.github.com/repos/cantino/mcfly/releases|grep browser_download_url|grep download|grep x86_64|grep linux|head -n1|cut -d '"' -f4)"
 sudo tar fxz mcfly-*-x86_64-*-linux-musl.tar.gz -C /usr/bin/
 cat <<EOF |tee "$HOME"/.bashrc.d/mcfly.bash>/dev/null
 #export MCFLY_LIGHT=TRUE
