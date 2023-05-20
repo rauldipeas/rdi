f0c97353026503db1646f62d8b4be2f7ca2879d1
Raul Dipeas <rdipeas+github@proton.me>
2023-05-20 03:57:03 -0300

    ^retroarch

--- scripts/retroarch.sh
+++ scripts/retroarch.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Instalação do EmulationStation
-wget -q --show-progress -O emulationstation-de_x64.deb $(wget -qO- https://es-de.org|grep x64.deb|head -n1|cut -d '=' -f4|cut -d '>' -f1)
+wget -cq --show-progress -O emulationstation-de_x64.deb "$(wget -qO- https://es-de.org|grep x64.deb|head -n1|cut -d '=' -f4|cut -d '>' -f1)"
 pkcon install-local emulationstation-de_x64.deb
 
 # Instalação do tema EpicNoir para o EmulationStation
