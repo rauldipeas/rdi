493d8a549040ce276e6721a4bc2a2421e8adb145
Raul Dipeas <rdipeas+github@proton.me>
2023-05-20 03:56:28 -0300

    ^appgrid

--- scripts/appgrid.sh
+++ scripts/appgrid.sh
@@ -2,5 +2,5 @@
 set -e
 
 # Instalação do App Grid
-wget -q --show-progress "$(wget -qO- https://www.appgrid.org|grep deb|cut -d "'" -f4)"
+wget -cq --show-progress "$(wget -qO- https://www.appgrid.org|grep deb|cut -d "'" -f4)"
 pkcon install-local ./appgrid*.deb
