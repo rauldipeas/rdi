88d394a73cab966a37027d6221f82c2e7a242523
Raul Dipeas <rdipeas+github@proton.me>
2023-05-21 00:50:41 -0300

    +tor-browser

---
+++ scripts/tor-browser.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+# Instalação do Tor
+wget -cq --show-progress -O tor-browser-linux64-stable_ALL.tar.xz https://www.torproject.org/dist/torbrowser/12.0.6/tor-browser-linux64-12.0.6_ALL.tar.xz
+tar fvx tor-browser-linux64-stable_ALL.tar.xz
+sudo mkdir -p /opt/tor-browser
+sudo chmod 777 /opt/tor-browser
+mv tor-browser/* /opt/tor-browser/
+sudo mkdir -p /usr/local/share/applications /usr/local/share/pixmaps
+sudo ln -fs /opt/tor-browser/Browser/browser/chrome/icons/default/default128.png /usr/local/share/pixmaps/tor-browser.png
+rm -rf tor-browser*
+cat <<EOF |sudo tee /opt/tor-browser/Browser/defaults/pref/csd.js>/dev/null
+pref("browser.tabs.inTitlebar", 1);
+EOF
+cat <<EOF |sudo tee /usr/local/share/applications/tor-browser.desktop>/dev/null
+[Desktop Entry]
+Type=Application
+Name=Tor Browser
+GenericName=Web Browser
+Comment=Tor Browser  is +1 for privacy and −1 for mass surveillance
+Categories=Network;WebBrowser;Security;
+Exec=sh -c '"/opt/tor-browser/Browser/start-tor-browser" --detach || ([ !  -x "/opt/tor-browser/Browser/start-tor-browser" ] && "$(dirname "$*")"/Browser/start-tor-browser --detach)' dummy %k
+X-TorBrowser-ExecShell=./Browser/start-tor-browser --detach
+Icon=tor-browser
+StartupWMClass=Tor Browser
+EOF
