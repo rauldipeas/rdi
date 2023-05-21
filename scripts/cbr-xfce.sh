c2306e3928e9ae0a7fce1abb90e40b2a63c8ab89
Raul Dipeas <rdipeas+github@proton.me>
2023-05-21 00:59:28 -0300

    ^cbr-xfce

--- scripts/cbr-xfce.sh
+++ scripts/cbr-xfce.sh
@@ -4,7 +4,7 @@ set -e
 # Miniaturas de arquivos cbr no XFCE
 if [ -f /usr/share/xsessions/xubuntu.desktop ]; then
     if [ -f /usr/share/thumbnailerers/atril.thumbnailer ]; then
-        cp -v /usr/share/thumbnailers/atril.thumbnailer .local/share/thumbnailers/
+        cp /usr/share/thumbnailers/atril.thumbnailer .local/share/thumbnailers/
         sed -i 's@xpsdocument;@xpsdocument;application/x-rar;@g' .local/share/thumbnailers/atril.thumbnailer
     fi
 fi
