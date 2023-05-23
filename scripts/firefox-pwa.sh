6d2bc2ccd1edd092dbdd84ec0bd2ac1ce611a5d5
Raul Dipeas <rdipeas+github@proton.me>
2023-05-23 16:08:02 -0300

    ^firefox-pwa

--- scripts/firefox-pwa.sh
+++ scripts/firefox-pwa.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Instalação do Firefox PWA
-wget -qO- https://packagecloud.io/filips/FirefoxPWA/gpgkey|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/firefoxpwa-keyring.gpg>/dev/null
+wget -qO- https://packagecloud.io/filips/FirefoxPWA/gpgkey|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/firefoxpwa-keyring.gpg
 echo 'deb https://packagecloud.io/filips/FirefoxPWA/any any main'|sudo tee /etc/apt/sources.list.d/firefoxpwa.list>/dev/null
 pkcon refresh
 sudo apt install --no-install-recommends firefoxpwa
