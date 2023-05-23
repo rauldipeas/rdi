6c5541fd6fb90f1fc6874b737b9d805071632d13
Raul Dipeas <rdipeas+github@proton.me>
2023-05-23 16:07:52 -0300

    ^brave

--- scripts/brave.sh
+++ scripts/brave.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Instalação do Brave
-wget -qO- https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/brave-browser-archive-keyring.gpg>/dev/null
+wget -qO- https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/brave-browser-archive-keyring.gpg
 echo 'deb https://brave-browser-apt-release.s3.brave.com/ stable main'|sudo tee /etc/apt/sources.list.d/brave-browser-release.list>/dev/null
 pkcon refresh
 pkcon install brave-browser
