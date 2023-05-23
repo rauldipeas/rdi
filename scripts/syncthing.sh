a692947f4dae3f305735f05a81767fd0b5bf11c0
Raul Dipeas <rdipeas+github@proton.me>
2023-05-23 16:09:44 -0300

    ^syncthing

--- scripts/syncthing.sh
+++ scripts/syncthing.sh
@@ -5,4 +5,4 @@ set -e
 wget -qO- https://syncthing.net/release-key.gpg|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/syncthing-archive-keyring.gpg
 echo 'deb https://apt.syncthing.net/ syncthing stable'|sudo tee /etc/apt/sources.list.d/syncthing.list>/dev/null
 pkcon refresh
-pkcon install syncthing
+pkcon install syncthing syncthing-gtk
