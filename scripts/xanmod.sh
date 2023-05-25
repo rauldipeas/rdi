5623f5cbb1f5c0caba54f785a066165d9bd1df9c
Raul Dipeas <rdipeas+github@proton.me>
2023-05-25 14:08:26 -0300

    ^xanmod

--- scripts/xanmod.sh
+++ scripts/xanmod.sh
@@ -3,7 +3,7 @@ set -e
 
 # Instalação do XanMod
 wget -qO- https://dl.xanmod.org/archive.key|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/xanmod-archive-keyring.gpg
-echo 'deb http://deb.xanmod.org releases main'|sudo tee /etc/apt/sources.list.d/xanmod-release.list
+echo 'deb http://deb.xanmod.org releases main'|sudo tee /etc/apt/sources.list.d/xanmod-release.list>/dev/null
 pkcon refresh
 wget -q --show-progress https://dl.xanmod.org/check_x86-64_psabi.sh
 chmod +x check_x86-64_psabi.sh
