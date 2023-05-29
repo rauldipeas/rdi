0acc8214a79866a26ec46372b2aabefb7915ee86
Raul Dipeas <rdipeas+github@proton.me>
2023-05-29 00:44:58 -0300

    ^apx

--- scripts/apx.sh
+++ scripts/apx.sh
@@ -9,6 +9,3 @@ sudo sed -i 's@share/apx@bin@g' /etc/apx/config.json
 
 # Criação do container AUR
 apx init --aur
-
-# Solução temporária para o erro com o yay
-# xdg-open https://github.com/Vanilla-OS/apx/issues/210#issuecomment-1523630181
