84f9ed0ba8b8cce3625e0956dfc122c94c30985e
Raul Dipeas <rdipeas+github@proton.me>
2023-06-02 13:57:08 -0300

    ^keybase

--- scripts/keybase.sh
+++ scripts/keybase.sh
@@ -6,4 +6,5 @@ wget -qO- https://keybase.io/docs/server_security/code_signing_key.asc|sudo gpg 
 echo 'deb http://prerelease.keybase.io/deb stable main'|sudo tee /etc/apt/sources.list.d/keybase.list>/dev/null
 pkcon update
 pkcon install keybase
+#keybase config set mountdir /media/"$USER"/"$DEVIE"/Keybase
 #ln -sv /keybase/private/"$USER" "$HOME"/Keybase
