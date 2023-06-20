5fabce59d5d29550d711c726c69114c364aa9475
Raul Dipeas <rdipeas+github@proton.me>
2023-06-20 00:52:28 -0300

    ^konsave

--- scripts/konsave.sh
+++ scripts/konsave.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Depende do pipx
-#xdg-open https://github.com/rauldipeas/rdi/blob/main/scripts/pipx.sh
+#xdg-open https://github.com/rauldipeas/rdi/blob/main/scripts/bashrc.d/pipx.sh
 
 # Instalação do konsave
 if [ -f /usr/share/xsessions/plasma.desktop ]; then
