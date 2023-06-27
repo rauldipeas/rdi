e34f8cef28aa4582214cb72b445ddccaeff622fe
Raul Dipeas <rdipeas+github@proton.me>
2023-06-27 01:45:41 -0300

    ^mangohud

--- scripts/mangohud.sh
+++ scripts/mangohud.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
+# Depende do Pacstall
+#xdg-open https://github.com/rauldipeas/rdi/blob/main/scripts/pacstall.sh
+
 # Instalação do MangoHud
-pacstall -I mangohud
-#wget -qc "$(wget -qO- https://api.github.com/repos/flightlessmango/MangoHud/releases|grep browser_download_url|grep download/v|grep tar.gz|head -n1|cut -d '"' -f4)"
-#tar fxz MangoHud*.tar.gz
-#cd MangoHud
-#./mangohud-setup.sh install
-#cd ..
-#rm -rf MangoHud
+wget -qc "$(wget -qO- https://api.github.com/repos/flightlessmango/MangoHud/releases|grep browser_download_url|grep download/v|grep tar.gz|head -n1|cut -d '"' -f4)"
+tar fxz MangoHud*.tar.gz
+cd MangoHud
+./mangohud-setup.sh install
+cd ..
+rm -rf MangoHud MangoHud*.tar.gz
+#pacstall -I mangohud
