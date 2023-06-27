805f842d0f2f6adc4ed4511b8156a6b75a8083f9
Raul Dipeas <rdipeas+github@proton.me>
2023-06-27 07:13:39 -0300

    ^apx

--- scripts/apx.sh
+++ scripts/apx.sh
@@ -4,10 +4,12 @@ set -e
 # Depende do Pacstall
 #xdg-open https://github.com/rauldipeas/rdi/blob/main/scripts/pacstall.sh
 
+# Depende do Distrobox
+#xdg-open https://github.com/rauldipeas/rdi/blob/main/scripts/bashrc.d/distrobox.sh
+
 # Instalação do apx
 pkcon install docker.io
 pacstall -I apx-git
-pacstall -I distrobox
 sudo sed -i 's@share/apx@bin@g' /etc/apx/config.json
 
 # Criação do container AUR
