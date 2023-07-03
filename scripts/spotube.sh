74ac8a00cd578e4edbf59b43607c81422b66ef09
Raul Dipeas <rdipeas+github@proton.me>
2023-07-03 13:14:15 -0300

    ^spotube

--- scripts/spotube.sh
+++ scripts/spotube.sh
@@ -3,4 +3,4 @@ set -e
 
 # Instalação do Spotube
 bash <(wget -qO- https://raw.githubusercontent.com/rauldipeas/apt-repository/main/apt-repository.sh)
-pkcon install spotube
+pkcon install libjsoncpp25 spotube
