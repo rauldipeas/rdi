115831b2a5c50c237b7066df5f04fec28b115a97
Raul Dipeas <rdipeas+github@proton.me>
2023-07-05 14:47:09 -0300

    ^rustdesk

--- scripts/rustdesk.sh
+++ scripts/rustdesk.sh
@@ -4,3 +4,8 @@ set -e
 # Instalação do RustDesk
 bash <(wget -qO- https://raw.githubusercontent.com/rauldipeas/apt-repository/main/apt-repository.sh)
 pkcon install rustdesk
+
+# Correção no ícone do lançador
+cat <<EOF |sudo tee /etc/apt/apt.conf.d/100rustdesk-icon>/dev/null
+DPkg::Post-Invoke {"sed -i 's@Icon=/usr/share/rustdesk/files/rustdesk.png@Icon=rustdesk@g' /usr/share/applications/rustdesk.desktop";};
+EOF
