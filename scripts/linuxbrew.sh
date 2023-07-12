a2b9e4ce63d574a3035f897104a3be6d00074277
Raul Dipeas <rdipeas+github@proton.me>
2023-07-12 17:20:45 -0300

    ^linuxbrew

--- scripts/linuxbrew.sh
+++ scripts/linuxbrew.sh
@@ -5,6 +5,6 @@ set -e
 bash <(wget -qO- https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
 
 # Configuração para o bash
-cat <<EOF |sudo tee /etc/X11/Xsession.d/99linuxbrew 
+cat <<EOF |sudo tee /etc/X11/Xsession.d/99linuxbrew>/dev/null
 eval "\$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 EOF
