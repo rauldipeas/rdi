6cc6d0843b021c35335c56382ebe154e97b56c06
Raul Dipeas <rdipeas+github@proton.me>
2023-07-20 02:22:50 -0300

    ^beeper

--- scripts/beeper.sh
+++ scripts/beeper.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -e
 
+# Depende do LinuxBrew
+#xdg-open https://github.com/rauldipeas/rdi/blob/main/scripts/bashrc.d/linuxbrew.sh
+
 # Instalação do Beeper
 bash <(wget -qO- https://raw.githubusercontent.com/rauldipeas/apt-repository/main/apt-repository.sh)
 pkcon install beeper
+
+# Correção dos ícones na área de notificação
+if [ -d /usr/share/icons/Papirus ]; then
+    brew install npm
+    npx asar e /opt/beeper/resources/webapp.asar beeper-asar
+    rsvg-convert -f png /usr/share/icons/Papirus/24x24/panel/messenger-indicator.svg -o beeper-asar/vector-icons/favicon.*.ico
+    npx asar p beeper-asar webapp.asar
+    sudo cp -rf webapp.asar /opt/beeper/resources/
+    sudo rsvg-convert -f png -h 256 -w 256 /usr/share/icons/Papirus/24x24/panel/messenger-indicator.svg -o /opt/beeper/resources/icons/icon.png
+    rm -rf beeper-asar webapp.asar
+fi
