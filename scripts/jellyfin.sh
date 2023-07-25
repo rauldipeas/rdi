0725d3be4f08c765478611db3ccb5f7f30c11155
Raul Dipeas <rdipeas+github@proton.me>
2023-07-25 01:58:06 -0300

    +jellyfin

---
+++ scripts/jellyfin.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Instalação do Jellyfin
+DISTRO="$(awk -F'=' '/^ID=/{ print $NF }' /etc/os-release)"
+CODENAME="$(awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release)"
+wget -qO- --show-progress https://repo.jellyfin.org/"${DISTRO}"/jellyfin_team.gpg.key|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/jellyfin.gpg
+cat <<EOF |sudo tee /etc/apt/sources.list.d/jellyfin.sources
+Types: deb
+URIs: https://repo.jellyfin.org/${DISTRO}
+Suites: ${CODENAME}
+Components: main
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/trusted.gpg.d/jellyfin.gpg
+EOF
+pkcon refresh
+pkcon install jellyfin
+sudo systemctl enable jellyfin.service
+sudo systemctl start jellyfin.service
