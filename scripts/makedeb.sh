70288ac9a99f1c658e805de409fca5b58e235b0a
Raul Dipeas <rdipeas+github@proton.me>
2023-08-08 10:56:03 -0300

    ^makedeb

--- scripts/makedeb.sh
+++ scripts/makedeb.sh
@@ -9,3 +9,5 @@ bash <(wget -qO- https://shlink.makedeb.org/install)
 wget -qO- https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub|gpg --dearmor -o /etc/apt/trusted.gpg.d/prebuilt-mpr-archive-keyring.gpg
 echo "deb [arch=amd64] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)"|sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list>/dev/null
 pkcon refresh
+pkcon install mist
+mist update
