447e16f833e2acb0b37115d93d4dffb1343b479d
Raul Dipeas <rdipeas+github@proton.me>
2023-08-08 10:43:22 -0300

    ^makedeb

--- scripts/makedeb.sh
+++ scripts/makedeb.sh
@@ -4,3 +4,8 @@ set -e
 # Instalação do MakeDeb
 export MAKEDEB_RELEASE='makedeb'
 bash <(wget -qO- https://shlink.makedeb.org/install)
+
+# Instalação do MPR
+wget -qO- https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub|gpg --dearmor -o /etc/apt/trusted.gpg.d/prebuilt-mpr-archive-keyring.gpg
+echo "deb [arch=amd64] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)"|sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list>/dev/null
+pkcon refresh
